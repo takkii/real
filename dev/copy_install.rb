afd33f1ba36e95251777c6b50a93828eb0678ae7
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-02-26 17:26:15 +0900

    Update.

---
+++ dev/copy_install.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+class RubyMethod
+  def initialize
+    @methods = Class.methods + Object.methods
+  end
+
+  def write_to_file
+    File.open('method.txt', 'a:utf-8', perm = 0o777) do |f|
+      f.puts @methods.sort.uniq
+    end
+  end
+end
+
+RubyMethod.new.write_to_file
