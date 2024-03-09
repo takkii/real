7823bdb7f9dd37412ac74c8726ae945b8b9afd2e
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-03-09 17:15:49 +0900

    Update.

--- dict/ruby_method.rb
+++ dict/ruby_method.rb
@@ -1,15 +1,36 @@
-#!/usr/bin/ruby
+# frozen_string_literal: true
 
+# The new instance will be deleted after process ends.
 class RubyMethod
+  attr_reader :methods
+
+  # Defaut encoding utf8, Encoding change here.
+  def encoding_style
+    Encoding.default_internal = 'UTF-8'
+    Encoding.default_external = 'UTF-8'
+  end
+
   def initialize
     @methods = Class.methods + Object.methods
   end
 
-  def write_to_file
+  def remove
+    encoding_style
     File.open('method.txt', 'a:utf-8', perm = 0o777) do |f|
       f.puts @methods.sort.uniq
     end
+    remove_instance_variable(:@methods)
   end
 end
 
-RubyMethod.new.write_to_file
+# About Exception, begin ~ rescue ~ ensure.
+begin
+  RubyMethod.new.remove
+  puts 'Exported, The RubyMethod to a Text File.'
+rescue StandardError => e
+  puts e.backtrace
+ensure
+  GC.compact
+end
+
+__END__
